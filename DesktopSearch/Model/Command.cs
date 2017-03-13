9736b91917fe9d04c990c882d269681211846521
crimean-s <insarabuz@gmail.com>
2017-03-14 00:10:34 +0300

    Добавлены папки

---
+++ DesktopSearch/Model/Command.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopSearch.Model
+{
+    public class Command
+    {
+        private string name;
+
+        public string Name { get; set; }
+    }
+}
---
+++ DesktopSearch/Model/SuggestResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace DesktopSearch.Model
+{
+    public class SuggestResult : INotifyPropertyChanged
+    {
+        private string content;
+        private Command command;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
